505124be31fb427f151a7f6c314999f47144ce3c
tcym <1124288757@qq.com>
2022-03-24 17:32:12 +0800

    km ti

--- src/main/java/com/example/xyz/algorithm/code/c_erf_03.java
+++ src/main/java/com/example/xyz/algorithm/code/c_erf_03.java
@@ -9,12 +9,6 @@ public class c_erf_03 {
 
     public static void main(String[] args) {
         int arr[] = new int[]{6, 10, 6, 6, 4, 4, 12, 4, 3, 3, 12, 12, 3};
-        // 6   00110
-        // 10  01010
-        // 4   00100
-        // 12  01100
-        // 3   00011
-
         System.out.println(km(arr, 1, 3));
     }
 
